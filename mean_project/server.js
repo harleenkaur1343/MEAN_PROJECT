5856edfa764ddbd465abf5479a4765af90882d54
Harleen Kaur <harleenkaur1343@gmail.com>
2023-04-25 13:33:43 +0530

    Update server.js

--- mean_project/server.js
+++ mean_project/server.js
@@ -1,37 +1,115 @@
 const express = require('express');
-//importing the router
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const studentRouter = require('./routers/exproutes');
-const app = express();
 const Student = require('./app/models/students');
+const db = require('./config/db');
 
+const app = express();
 //const mongoose = require('mongoose');
-const port = 3000;
-
-var db = require('./config/db');
+const port = 5000;
 console.log("connecting..",db);
 
+app.use(cors());
 //Returns middleware that only parses json and only looks at requests where the Content-Type header matches the type option.
 app.use(express.json());
+
 app.use(studentRouter);
-//mongoose.connect(db.url);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended:false
+}));
+
+
+/*const corsOpts = {
+    origin: '*',
+  
+    methods: [
+      'GET',
+      'POST',
+    ],
+  
+    allowedHeaders: [
+      'Content-Type',
+    ],
+  };*/
+  
+  //app.use(cors({origin : '*'}));
+/* app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+ 
+app.options( '*' , cors()) */
+
+/*app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});*/
+
+//app.use(express.static(path.join(__dirname,'dist/Studentt')));
 
 app.get('/',(req, res)=> res.send('Express app running'));
 
-/*function(err) {
-        if (err)
-        res.send(err);
-        res.json({ message: 'student created!' });
-        }*/
-app.post('/api/students/send', function (req, res) {
-    console.log("Processing request 1");
-    var student = new Student(); // create a new instance of the student model
-    student.name = req.body.name; // set the student name (comes from the request)
-    console.log("Processing request 2");
-    student.save();
-    console.log("Processing request 3");
-});
+app.post('/api/students/send',(req,res)=>{
 
-app.delete('/api/students/:student_id', function (req, res) {
+    console.log(req.body)
+    let studt = new Student({
+        name : req.body.name,
+        place : req.body.place,
+        country : req.body.country
+    });
+
+    studt.save().then((doc)=>{
+        console.log(doc);
+    }).catch((err)=>{
+        console.log(err);
+    })
+});
+app.put('/api/students/update',(req,res)=>{
+
+    Student.findOneAndUpdate(
+        {
+            _id : req.body.id
+        },
+        {
+            name : req.body.name,
+            place : req.body.place,
+            country : req.body.country
+        },
+        {
+          new: true, // return updated doc
+          runValidators: true // validate before update
+        }
+      )
+        .then((doc) => {
+          console.log(doc);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+});
+app.delete('/api/students/delete',(req,res)=>{
+    
+    console.log(req.body.id);
+    //let  studt = new Student();
+    Student.findOneAndDelete({
+        _id : req.body.id
+    })
+    .then((response)=>{console.log(response);})
+    .catch((err)=>{
+        console.log(err);
+    });
+});
+/*app.delete('/api/students/:student_id', function (req, res) {
     //var student = new Student(); // create a new instance of the student model
 
     try{
@@ -44,26 +122,7 @@ app.delete('/api/students/:student_id', function (req, res) {
         res.send(err);
     }
 
-});
-//defining route
-/*app.get('/tproute', function (req, res) {
-    res.send('This is routing for the application developed using Node and Express...')});
+});*/
 
-
-// sample api route
-// grab the student model we just created
-var Student = require('./app/models/students');
-app.get('/api/students', function(req, res) {
-// use mongoose to get all students in the database
-Student.find().then(function(err, students) {
-// if there is an error retrieving, send the error.
-// nothing after res.send(err) will execute
-if (err)
-res.send(err);
-res.json(students); // return all students in JSON format
-});
-});
-// startup our app at http://localhost:3000
-//app.listen(port, () ⇒ console.log(`Example app listening on port ${port}!`));*/
 
 app.listen(port, ()=>console.log(`Example app listening on port ${port}!`));
